0ef1f747bb87cf6cfe36c8f075a4874aaeb24401
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-07-05 22:19:25 -0500

    edited html and Js
    Set the current day and time

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
 
 
+var interval = setInterval(function() {
+  var momentNow = moment();
+  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                      + momentNow.format('dddd')
+                       .substring(0,3).toUpperCase());
+  $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+}, 100);
 
 
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
